837dbc79220b8cec621e419caf7ab070278140ab
ryanosaur <ryan.justin.taylor@gmail.com>
2015-05-08 12:43:28 -0700

    updated challenge 5 to use ternary

--- 5-case-swap/caseSwap.js
+++ 5-case-swap/caseSwap.js
@@ -2,14 +2,7 @@ function caseSwap(textToSwap){
     var swappedText = "";
     for(var i = 0; i < textToSwap.length; i++)
     {
-        if(textToSwap[i] === textToSwap[i].toLowerCase())
-        {
-            swappedText += textToSwap[i].toUpperCase();
-        }
-        else
-        {
-            swappedText += textToSwap[i].toLowerCase();
-        }
+        swappedText += textToSwap[i] === textToSwap[i].toLowerCase() ? textToSwap[i].toUpperCase() : textToSwap[i].toLowerCase();
     }
     return swappedText;
 }
