da8a3bd8ae96d3c06b4317eba200e73f2eddaec7
ryanosaur <ryan.justin.taylor@gmail.com>
2015-05-23 19:46:41 -0700

    complete challenge 13

---
+++ 13-jquery-dom/div-colorer.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+    $("div").each(function(index){
+        var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        $(this).css("background-color", randomColor);
+        $(this).css("color", invertColor(randomColor));
+    });
+});
+
+var invertColor = function(hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+};
